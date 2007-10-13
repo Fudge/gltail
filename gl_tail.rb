5d44e3de9aa6762df938a230b0b2c17493fc5936
erlends <erlends>
2007-10-13 12:39:46 +0000

    Split configuration into separate file.

--- gl_tail.rb
+++ gl_tail.rb
@@ -15,57 +15,6 @@
 #   Geolocation on IPS
 #
 
-#################
-### Configuration
-###
-
-ENV['__GL_SYNC_TO_VBLANK']="1"
-$WINDOW_WIDTH = 1200
-$WINDOW_HEIGHT = 760
-
-$COLUMN_SIZE_LEFT  = 25 # in characters, will be truncated
-$COLUMN_SIZE_RIGHT = 25 # in characters, will be truncated
-
-$RIGHT_COL = 0.99
-$LEFT_COL = -0.99
-
-$MIN_BLOB_SIZE = 0.004
-$MAX_BLOB_SIZE = 0.04
-
-$SERVERS = [
-            # List of machines to log in to via SSH, and which files to tail for traffic data.
-            {:name => 'server1', :host => 'server1.example.com', :user => 'joeuser', :password => 'secret', :command => 'tail -f', :files => ['/var/log/apache/access_log'], :color => [0.2, 1.0, 0.2, 1.0], :parser => :apache },
-            {:name => 'server2', :host => 'login.mycoolsite.com', :user => 'otheruser', :password => 'othersecret', :port => 22222, :command => 'xtail', :files => ['/usr/local/www/apps/myapp/current/log/production.log'], :color => [0.2, 0.2, 1.0, 1.0], :parser => :rails },
-            {:name => 'mail', :host => 'mail.spamme.com', :user => 'otheruser', :password => 'othersecret', :command => 'tail -f', :files => ['/var/log/maillog'], :color => [0.8, 1.0, 0.0, 1.0], :parser => :postfix },
-            {:name => 'database', :host => 'db.example.com', :user => 'db', :password => 'othersecret', :command => 'tail -f', :files => ['/var/log/pgsql.log'], :color => [0.6, 0.6, 1.0, 1.0], :parser => :postgresql },
-           ]
-
-$BLOCKS = [
-           # Sections with different information to display on the screen
-           { :name => 'info',          :position => :left,  :order => 0, :size => 10, :auto_clean => false, :show => :total },
-           { :name => 'hosts',         :position => :left,  :order => 1, :size => 3  },
-           { :name => 'sites',         :position => :left,  :order => 2, :size => 10 },
-           { :name => 'content',       :position => :left,  :order => 3, :size => 5,  :show => :total, :color => [1.0, 0.8, 0.4, 1.0] },
-           { :name => 'status',        :position => :left,  :order => 4, :size => 10, :color => [1.0, 0.8, 0.4, 1.0] },
-           { :name => 'types',         :position => :left,  :order => 5, :size => 5,  :color => [1.0, 0.4, 0.2, 1.0] },
-           { :name => 'users',         :position => :left,  :order => 6, :size => 10 },
-           { :name => 'smtp',          :position => :left,  :order => 7, :size => 5  },
-           { :name => 'logins',        :position => :left,  :order => 8, :size => 5  },
-           { :name => 'database',      :position => :left,  :order => 9, :size => 10 },
-
-           { :name => 'urls',          :position => :right, :order => 0, :size => 15 },
-           { :name => 'slow requests', :position => :right, :order => 1, :size => 5, :show => :average },
-           { :name => 'referrers',     :position => :right, :order => 2, :size => 10 },
-           { :name => 'user agents',   :position => :right, :order => 3, :size => 5, :color => [1.0, 1.0, 1.0, 1.0] },
-           { :name => 'mail from',     :position => :right, :order => 4, :size => 5  },
-           { :name => 'mail to',       :position => :right, :order => 5, :size => 5  },
-           { :name => 'viruses',       :position => :right, :order => 6, :size => 5  },
-           { :name => 'rejections',    :position => :right, :order => 7, :size => 5, :color => [1.0, 0.2, 0.2, 1.0]  },
-           { :name => 'warnings',      :position => :right, :order => 8, :size => 5  },
-          ]
-###
-### Configuration end
-######################
-
+require 'config.rb'
 require 'lib/gl_tail.rb'
 GlTail.new.start
