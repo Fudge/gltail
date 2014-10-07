0cb3af70aea16d4f1e941e72af27d8ad4742b476
Christian Kakesa <christian.kakesa@gmail.com>
2014-10-07 02:13:09 +0200

    Update Named parser.

--- lib/gl_tail/parsers/named.rb
+++ lib/gl_tail/parsers/named.rb
@@ -22,37 +22,21 @@
 #-----------------------------------------------------------------------------------------------------------#
 
 class NamedParser < Parser
-  def parse( line )
-    _, date, time, host, port, view, query, type, type2, _ = /(\d+-\w+-\d+) (\d+:\d+:\d+\.\d+) client (\d+\.\d+\.\d+\.\d+)#(\d+) \((.+)\): query: (.*) (.*) (.*) (.*)/.match(line).to_a
-    
-    if host
-      add_activity(block: 'sites', name: source.name)
-      add_activity(block: 'hosts', name: host, size: 1)
-      add_activity(block: 'types', name: "#{type} #{type2}")
-
-      add_activity(:block => 'dns query', :name => query, :size => set_type_size(type))
-      # add_activity(:block => 'port', :name => port, :size => 1, :type => 3)
-      # add_activity(:block => 'time', :name => time, :size => 1, :type => 3)
-      # add_activity(:block => 'view', :name => view, :size => 1, :type => 3)
-      # user_id = Digest::MD5.hexdigest(host)
-      # add_activity(:block => 'dns_user_id', :name => user_id, :size => set_type_size(type))
+  def parse(line)
+    case line.to_s
+      when /(\d+-\w+-\d+) (\d+:\d+:\d+\.\d+) client (?<host>\d+\.\d+\.\d+\.\d+)#(\d+) \((.+)\): query: (?<query>.+) (?<type>\S+) (?<type2>\S+) \((.*)\)/
+        add_activity(block: 'sites',     name: source.name)
+        add_activity(block: 'types',     name: "#{$~[:type]} #{$~[:type2]}")
+        add_activity(block: 'hosts',     name: $~[:host],  size: 1)
+        add_activity(block: 'dns query', name: $~[:query], size: set_type_size($~[:type]))
+      else
+        # ...
     end
-    
   end   
        
   # you can use size to distinguish request types
   def set_type_size(type)
-    case type
-    when 'A' then 10
-    when 'PTR' then 90
-    when 'AAAA' then 70
-    when 'TXT' then 60
-    when 'SOA' then 50
-    when 'MX' then 40
-    when 'SRV' then 30
-    when 'ANY' then 100
-    else 
-      150
-    end
+    @type_size ||= {A: 30, SRV: 40, MX: 50, SOA: 60, TXT: 70, AAAA: 80, PTR: 90, ANY: 100}
+    @type_size[type.to_sym]||150
   end
 end
